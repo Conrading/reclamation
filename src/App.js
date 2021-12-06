0140b41d92e9afe5761a7ebbb3d5734758067192
conrading <trafficking22@gmail.com>
2021-12-06 18:10:00 +0100

    pto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MainPage extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      //
+    }
+  }
+  render () {
+    return (
+      <body>
+        <div className="main-frame">
+          <div className="margin-center ">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <div className="flip-card-text-gap">What?</div>
+                </div>
+                <div class="flip-card-back">
+                  <h1>Lin Yang-Yu</h1> 
+                  <p>is</p> 
+                  <p>having PTO</p>
+                </div>
+              </div>
+            </div>
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <div className="flip-card-text-gap">When?</div>
+                </div>
+                <div class="flip-card-back">
+                  <h1>John Doe</h1> 
+                  <p>Architect & Engineer</p> 
+                  <p>We love that guy</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    )
+  }
 }
 
-export default App;
+export default MainPage;
