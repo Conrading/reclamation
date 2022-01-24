85e028423f3852472f5aa40da0c727e902ee5dba
conrading <trafficking22@gmail.com>
2022-01-24 18:13:28 +0100

    type effect

--- src/App.js
+++ src/App.js
@@ -5,96 +5,89 @@ class MainPage extends Component {
   constructor () {
     super ();
     this.state = {
-      //
+      anwortFullText: "Hi, I am ooo, from Wednesday Jan.26 to Thrusday Jan.27, will be back on Friday Jan.28",
+      step: null
     }
   }
   render () {
     return (
       <body>
         <div className="main-frame">
-          <div className="margin-center">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>My favorite Song in 2021</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <b className='text-pointer' onClick={() => {window.location = `https://www.youtube.com/watch?v=xR_Pa1LY8mc`}}>Ostatni</b>
-                  </div>
-                </div>
-              </div>
+          {this.state.step === null &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "erste" })}}>{this.state.anwortFullText}</div>
             </div>
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>Movie <b>Ad Astra</b> is inspried by</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <b className='text-pointer' onClick={() => {window.location = `https://en.wikipedia.org/wiki/Heart_of_Darkness`}}>Heart of Darkness</b>
-                  </div>
-                </div>
-              </div>
+          </div>}
+          <br />
+          {this.state.step === "erste" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "zweite" })}}>thanks for paying attention, umm.. thanks .. um..</div>
             </div>
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>largest City in 2021</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <b className='text-pointer' onClick={() => {window.location = `https://www.re-thinkingthefuture.com/city-and-architecture/a4608-15-largest-cities-as-of-2021/`}}>Tokyo</b>
-                  </div>
-                </div>
-              </div>
+          </div>}
+          <br />
+          {this.state.step === "zweite" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "dreite" })}}>Do you happen to know someone?</div>
             </div>
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>longest-running scripted USA TV serie</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <b className='text-pointer' onClick={() => {window.location = `https://en.wikipedia.org/wiki/List_of_longest-running_scripted_American_primetime_television_series`}}>the Simpsons</b>
-                  </div>
-                </div>
-              </div>
+          </div>}
+          <br />
+          {this.state.step === "dreite" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "viewrte" })}}>someone who plays music, any type, any instruments, I am looking for partner, just for fun</div>
             </div>
-          </div>
+          </div>}
+          <br />
+          {this.state.step === "viewrte" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "funfte" })}}>Just cover some songs, something catchy, something familiar</div>
+            </div>
+          </div>}
+          <br />
+          {this.state.step === "viewrte" &&
+          <div className='player-frame'>
+            <br />
+            <iframe width="511" height="100" scrolling="no" frameborder="no" allow="autoplay" className='player-center'
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1176083725&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+          </div>}
+          {this.state.step === "viewrte" &&
+          <div className='player-frame'>
+            <br />
+            <iframe width="1280" height="540" src="https://www.youtube.com/embed/I2dyKtSbsbM" className='player-center'
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>}
+          <br />
+          {this.state.step === "funfte" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "sechte" })}}>For sure some Jazz can be super cool, just don't know whether my piano can follow </div>
+            </div>
+          </div>}
+          {this.state.step === "funfte" &&
+          <div className='player-frame'>
+            <br />
+            <iframe width="510" height="315" src="https://www.youtube.com/embed/tpGOXpvqtbY" className='player-center'
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>}
+          <br />
+          {this.state.step === "sechte" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: "siebte" })}}>if you happen to know someone, who would be interested, that can be awesome</div>
+            </div>
+          </div>}
+          <br />
+          {this.state.step === "siebte" &&
+          <div className='jedes-linie-gesprach'>
+            <div className='jedes-linie-gesprach-inhalt'>
+              <div className='jedes-linie-gesprach-font text-pointer' onClick={() => {this.setState({ step: null })}}>..thanks</div>
+            </div>
+          </div>}
         </div>
-        <div className="main-frame">
-          <div className="second-margin-center">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>What?</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <b>Lin Yang-Yu</b> 
-                    <div>is having <b>PTO</b></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div>When?</div>
-                </div>
-                <div class="flip-card-back">
-                  <div>
-                    <div>from <b>1/13</b> Thursday</div>
-                    <div>to <b>1/14</b> Friday</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <div className='centeline-isolation'><hr /></div>
       </body>
     )
   }
