118cb864558f4d1436f6c7a8f03ca9b420ca27a7
conrading <trafficking22@gmail.com>
2022-06-16 15:47:27 +0200

    calendar

--- src/App.js
+++ src/App.js
@@ -1,84 +1,88 @@
-import { useState } from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import km from './km.json'
 
-function MainPage () {
-  const [showTable, setShowTable] = useState(true)
-  let currentPlay = 1
-  function switchPage (n) {
-    playPage(currentPlay += n)
-  }
-  function playPage (n) {
-    var i
-    const jedesBild = document.getElementsByClassName('slideShowjedesBild')
-    if (n > jedesBild.length) {currentPlay = 1}
-    if (n < 1) {currentPlay = jedesBild.length}
-    for (i = 0; i < jedesBild.length; i++) {
-      jedesBild[i].style.display = "none"
+
+class Kalendarz extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      każdy: [
+        {"month": "January", "start": 6, "last": 31, "pto": [], "bank": [1, 6]},
+        {"month": "February", "start": 2, "last": 28, "pto": [], "bank": []},
+        {"month": "March", "start": 2, "last": 31, "pto": [], "bank": []},
+        {"month": "April", "start": 5, "last": 30, "pto": [], "bank": [17]},
+        {"month": "May", "start": 6, "last": 31, "pto": [], "bank": [1, 3]},
+        {"month": "June", "start": 3, "last": 30, "pto": [24,25,26,27], "bank": [16]},
+        {"month": "July", "start": 5, "last": 31, "pto": [], "bank": []},
+        {"month": "August", "start": 1, "last": 31, "pto": [], "bank": [15]}
+      ],
+      remark: null
     }
-    jedesBild[currentPlay-1].style.display = "block"
   }
-  return (
-    <div>
-      {showTable === true && <div className='main-frame'>
-        <table className='updateTableFrame'>
-          <tr className='updateTableTitleUniversal'>
-            <th className='updateTableTitleErste'>Section</th>
-            <th>Field</th>
-            <th className='updateTableTitleErste'>Update from</th>
-          </tr>
-          <tr className='updateTableContent'>
-            <td className='updateTableContentJedes' onClick={() => {setShowTable(false)}}>{km.jedes[0].section}</td>
-            <td className='updateTableContentJedes' onClick={() => {setShowTable(false)}}>{km.jedes[0].field}</td>
-            <td className='updateTableContentJedes' onClick={() => {setShowTable(false)}}>{km.jedes[0].update}</td>
-          </tr>
-        </table>
-      </div>}
-      {showTable === false && 
-      <div className='main-frame'>
-        <div className='jedesUpdateFrame'>
-          <div className='makeRow'>
-            <div className='jedesUpdateFieldFrame'>
-              <div className='jedesUpdateFieldRow makeRow'>
-                <div className='jedesUpdateFieldTitle'>Update: </div>
-                <div className='jedesUpdateFieldImport'>{km.jedes[0].update}</div>
+  render () {
+    let miesiąc = this.state.każdy.map( i => {
+      let actualMonth = []
+      for (let j = 0; j < i.start; j++) {
+        actualMonth.push(null)
+      }
+      for (let k = 0; k < i.last; k++) {
+        actualMonth.push(k+1)
+      }
+      let każdegoDnia = actualMonth.map( d => {
+        let confirm = true
+        for (let p = 0; p < i.pto.length; p++) {
+          if (d === i.pto[p]) {
+            confirm = false
+            return(
+              <div className='dni-pto-text' onClick={() => {this.setState({remark: "I am on PTO from 24 to 27"})}}>{d}</div>
+            )
+          } 
+        }
+        for (let b = 0; b < i.bank.length; b++) {
+          if (d === i.bank[b]) {
+            confirm = false
+            return(
+              <div className='dni-bank-text'>{d}</div>
+            )
+          } 
+        }
+        if (confirm = true) {
+          return(
+            <div className='centeringText'>{d}</div>
+          )
+        }
+      })
+      return (
+        <div>
+          <div className='card-project'>
+              <div className='miesiąc'>{i.month}</div>
+              <div className="limit-width-sieben"><hr /></div>
+              <div className='tydzień'>
+                <div className='tydzień-sunday centeringText'>Sun</div>
+                <div className='centeringText'>Mon</div>
+                <div className='centeringText'>Tue</div>
+                <div className='centeringText'>Wed</div>
+                <div className='centeringText'>Thu</div>
+                <div className='centeringText'>Fri</div>
+                <div className='tydzień-saturday'>Sat</div>
               </div>
-              <div className='jedesUpdateFieldRow makeRow'>
-                <div className='jedesUpdateFieldTitle'>Section: </div>
-                <div className='jedesUpdateFieldImport'>{km.jedes[0].section}</div>
-              </div>
-              <div className='jedesUpdateFieldRow makeRow'>
-                <div className='jedesUpdateFieldTitle'>Field: </div>
-                <div className='jedesUpdateFieldImport'>{km.jedes[0].field}</div>
-              </div>
-              <div className='jedesUpdateFieldRow makeRow'>
-                <div className='jedesUpdateFieldTitle'>Choice: </div>
-                <div className='jedesUpdateFieldImport'>{km.jedes[0].option}</div>
-              </div>
-            </div>
-            <div className='jedesUpdateFieldFrame'>
-              <div className='jedesUpdateFieldRow'>
-                <div className='jedesUpdateFieldTitle'>Commend:</div>
-                <div>{km.jedes[0].commend}</div>
-              </div>
-            </div>
+              <div className="dni-container">{każdegoDnia}</div>
           </div>
-          <div className='slideShowFrame'>
-            <div className='sliderExample'>- Example -</div>
-            <div className='slideShowContainer'>
-              <img className='slideShowjedesBild' src="https://upload.wikimedia.org/wikipedia/commons/f/f1/An_example_for_a_nailed_note.jpg" />
-              <img className='slideShowjedesBild' src="https://upload.wikimedia.org/wikipedia/commons/e/ee/Blocksatz-Beispiel_deutsch%2C_German_text_sample_with_fully_justified_text.svg" />
-              <div className='sliderbottomFrame makeRow'>
-                <button className='silderSwitcher' onClick={() => {switchPage(-1)}}>❮</button>
-                <button className='silderSwitcher' onClick={() => {switchPage(1)}}>❯</button>
-              </div>
-            </div>
-          </div>
-          <div className='returnButton' onClick={() => {setShowTable(true)}}>Return</div>
         </div>
-      </div>}
-    </div>
-  )
+      )
+    })
+    return (
+      <body>
+        <div className='main-title'>2022 Calendar</div>
+        <div className='center-by-margin'>{miesiąc}</div>
+        <div className='remark' onClick={() => {this.setState({remark: null})}}>{this.state.remark}</div>
+        <div className='bottom-note'>
+          <div className='bottom-note-bank'>* Bank Holiday in Poland</div>
+          <div className='bottom-note-PTO'>* PTO duration</div>
+        </div>
+      </body>
+    )
+  }
 }
 
-export default MainPage;
+export default Kalendarz;
